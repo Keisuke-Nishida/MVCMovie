df9ba52bec4415602910494e13cf64de8c6173a2
keisuke.nishida <keisuke.nishida@frontarc.co.jp>
2021-03-11 18:01:00 +0900

    バリデーション　追加　チュートリアル完成

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,11 +6,24 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+
+        [Range(1, 10000)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+     
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        
     }
 }
